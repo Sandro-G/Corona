e4cd97b9c9760e7fcf177bb2ff5db1b3964ae493
Sandro <sandrogrieger@gmx.de>
2020-04-11 12:18:31 +0200

    Für Skriptausführung außerhalb R-Studio: working directory set

--- 2020-04-02_Corona_Daily_5_global.R
+++ 2020-04-02_Corona_Daily_5_global.R
@@ -5,7 +5,7 @@ library(purrr)
 library(grid)
 library(gridExtra)
 
-
+setwd("C:/Users/Sandro/Documents/R/Skripte/Corona")
 
 
 download.file("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
